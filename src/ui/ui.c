0ad2f0c38ba534d20a08222ff3c4ffd865810ec5
b-rodrigues <brodriguesco@protonmail.com>
2026-08-30 16:57:11 +0200

    fix(gfx): use first-row dagger (UID 9) and 6th-row rightmost shield (UID 64)

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -62,9 +62,9 @@ static const palette_color_t cgb_sprite_palette[4] = {
 
 static const uint16_t s_card_icon_uids[9] = {
     29,  /* ASSET_EQUIP_C04_R03: Iron Sword (UI_TILE_CARD_SWORD) */
-    87,  /* ASSET_EQUIP_C06_R08: Wooden Shield (UI_TILE_CARD_SHIELD) */
+    64,  /* ASSET_EQUIP_C19_R05: Shield 6th row rightmost (UI_TILE_CARD_SHIELD) */
     138, /* ASSET_EQUIP_C07_R11: Bow (UI_TILE_CARD_BOW) */
-    78,  /* ASSET_EQUIP_C07_R07: Dagger / Stiletto (UI_TILE_CARD_DAGGER) */
+    9,   /* ASSET_EQUIP_C07_R02: Dagger first weapon row (UI_TILE_CARD_DAGGER) */
     354, /* ASSET_SYM_C05_R04: Solitaire Diamond Ring (UI_TILE_CARD_RING) */
     149, /* ASSET_EQUIP_C07_R12: Amulet (UI_TILE_CARD_AMULET) */
     479, /* ASSET_SYM_C23_R16: Flame Spire (UI_TILE_CARD_ELEM_FIRE) */
